8bb6ae6e7fd11bb8075a4b550d27816cd3df8f68
Bui Trong Van <trongvan159357@gmail.com>
2024-12-18 22:16:52 +0700

    update port

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>("PORT") || 4000;
-  await app.listen(3000);
+  await app.listen(port);
 
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
