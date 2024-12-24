621777b3fa659f1bc423c45951a72c3867d085f1
Bui Trong Van <trongvan159357@gmail.com>
2024-12-24 15:40:49 +0700

    fix: add missing "summary" on Event

---
+++ prisma/migrations/20241224084017_update_summary/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isBot` on the `Message` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Event" ADD COLUMN     "summary" TEXT;
+
+-- AlterTable
+ALTER TABLE "Message" DROP COLUMN "isBot";
