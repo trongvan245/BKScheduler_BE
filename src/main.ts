a3def5c46f17175d4c4bdf024fd71c9c74fe036f
Bui Trong Van <trongvan159357@gmail.com>
2025-06-01 14:54:39 +0700

    endpoint

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix("api/v1");
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true, forbidNonWhitelisted: false }));
 
   const config = new DocumentBuilder()
@@ -19,14 +20,14 @@ async function bootstrap() {
     .addTag("Notifications")
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api", app, document, {
+  SwaggerModule.setup("swagger", app, document, {
     swaggerOptions: {
       persistAuthorization: true, // Optional: Keeps the token after refreshing the page
     },
   }); //https://github.com/nestjs/swagger/issues/92 add trailing slash to the end of the url
   const allowedOrigins = [
     "http://localhost:5173", // Frontend service 1 (development)
-    "http://localhost:3000", // Frontend service 2 (development)
+    "http://localhost:8081", // Frontend service 2 (development)
     "http://localhost:4000", // Frontend service 3 (development)
     "https://bkbotscheduler.vercel.app", // Frontend service 1 (production)
   ];
